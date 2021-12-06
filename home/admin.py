dc9d4318b8bea5275a9bf09fb3e5a0b7a6b06052
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2021-12-07 02:53:16 +0600

    backend minor bug fix

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from home.models import UserFeedback, UserModel
+from home.models import ResetPwdTokens, UserFeedback, UserModel
 
 # Register your models here.
 admin.site.register(UserModel)
+admin.site.register(ResetPwdTokens)
 admin.site.register(UserFeedback)
