cc249ccc9043dd1283d447b5022cbac4408dd48b
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2022-01-05 03:12:10 +0600

    pdf file download done

--- home/views.py
+++ home/views.py
@@ -8,68 +8,44 @@ import time
 import uuid
 from lost_and_found.mail_service import send_claim_acception_mail, send_claim_rejection_mail, send_forget_password_mail, send_point_purchase_mail, send_point_success_mail
 
-from django.http import HttpResponseRedirect
 from django.http import HttpResponse
-from django.http import FileResponse
-import io
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import inch
-from reportlab.lib.pagesizes import letter
+
+from django.template.loader import get_template
+
+from xhtml2pdf import pisa
 # Create your views here.
 
 # generate pdf
 
 
-def pdf(request):
+def pdf_generated(request):
     try:
         user = UserModel.objects.get(email=request.session['email'])
-        buf = io.BytesIO()
-        c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+        try:
+            claimer = ClaimOwner.objects.raw(
+                'SELECT * FROM claim_owner WHERE claimerEmail = %s and STATUS = %s;', [user.email, 'Accepted'])[0]
+            showUser = UserModel.objects.get(email=claimer.postPunlisherEmail)
+            post = PostModel.objects.get(id=claimer.postId)
 
-        # create a text object
-        textob = c.beginText()
-        textob.setTextOrigin(inch, inch)
-        textob.setFont("Helvetica", 11)
-
-        lines = [
-            user.name,
-            user.email,
-            user.phoneNumber,
-            user.bio,
-            user.point,
-            user.completeProfile,
-            user.location,
-            user.messengerUrl,
-            user.whatsappUrl,
-            user.telegramUrl,
-        ]
-
-        lines.append('\n\n')
-        lines.append(user.name)
-        lines.append(user.email)
-        lines.append(user.phoneNumber)
-        lines.append(user.bio)
-        lines.append(user.point)
-        lines.append(user.completeProfile)
-        lines.append(user.location)
-        lines.append(user.messengerUrl)
-        lines.append(user.whatsappUrl)
-        lines.append(user.telegramUrl)
-
-        # loop
-        for line in lines:
-            textob.textLine(line)
-
-        # Finish up
-        c.drawText(textob)
-        c.showPage()
-        c.save()
-        buf.seek(0)
-
-        return FileResponse(buf, as_attachment=True, filename='lost-and-found.pdf')
+            template_path = 'pdf_generated.html'
+            context = {'user': user, 'showUser': showUser, 'post': post}
+            response = HttpResponse(content_type='application/pdf')
+            # response['Content-Disposition'] = 'attachment; filename="lost-and-found.pdf"' # for download
+            response['Content-Disposition'] = 'filename="lost-and-found.pdf"'
+            template = get_template(template_path)
+            html = template.render(context)
+            # create a pdf
+            pisa_status = pisa.CreatePDF(
+                html, dest=response)
+            # if error then show some funy view
+            if pisa_status.err:
+                return HttpResponse('We had some errors <pre>' + html + '</pre>')
+            return response
+        except:
+            messages.error(request, 'You have no pdf to show.')
+            return redirect('/')
     except:
         messages.error(request, 'You need to login first')
-        print()
         return redirect('authenticate')
 
 
